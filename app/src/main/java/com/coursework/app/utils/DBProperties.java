69b5ebddf147694dce5f9d00405457571a4dc345
vladlukynov <lukynov.1@yandex.ru>
2023-01-09 06:40:17 +0300

    Fix unit tests

--- app/src/main/java/com/coursework/app/utils/DBProperties.java
+++ app/src/main/java/com/coursework/app/utils/DBProperties.java
@@ -1,7 +1,7 @@
 package com.coursework.app.utils;
 
 public class DBProperties {
-    public static final String URL = "jdbc:sqlserver://localhost\\MSSQLSERVER:1433;database=TradeOrganization;encrypt=true;trustServerCertificate=true;";
+    public static String URL = "jdbc:sqlserver://localhost\\MSSQLSERVER:1433;database=TradeOrganization;encrypt=true;trustServerCertificate=true;";
     public static final String userName = "tradeOrganization";
     public static final String password = "12345";
 }
